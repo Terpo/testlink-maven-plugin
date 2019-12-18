4fceede09f76081edeaabd17443d29fe191a7444
Terpo <mail@terpo.org>
2019-12-18 13:33:09 +0100

    apply formatter, add more info output, fix NPE for null builds

--- src/main/java/com/breinjhel/TestlinkMojo.java
+++ src/main/java/com/breinjhel/TestlinkMojo.java
@@ -1,187 +1,196 @@
 package com.breinjhel;
 
-import br.eti.kinoshita.testlinkjavaapi.TestLinkAPI;
-import br.eti.kinoshita.testlinkjavaapi.constants.ExecutionStatus;
-import br.eti.kinoshita.testlinkjavaapi.constants.ExecutionType;
-import br.eti.kinoshita.testlinkjavaapi.constants.TestCaseDetails;
-import br.eti.kinoshita.testlinkjavaapi.constants.TestLinkResponseParams;
-import br.eti.kinoshita.testlinkjavaapi.model.Build;
-import br.eti.kinoshita.testlinkjavaapi.model.TestCase;
-import br.eti.kinoshita.testlinkjavaapi.model.TestPlan;
-import br.eti.kinoshita.testlinkjavaapi.model.TestProject;
-import com.breinjhel.model.TestSuite;
-import com.google.inject.internal.util.Maps;
-import junit.framework.Test;
+import java.io.File;
+import java.io.FileFilter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
 import org.apache.commons.io.filefilter.WildcardFileFilter;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
-import java.io.File;
-import java.io.FileFilter;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
+import com.breinjhel.model.TestSuite;
+
+import br.eti.kinoshita.testlinkjavaapi.TestLinkAPI;
+import br.eti.kinoshita.testlinkjavaapi.constants.ExecutionStatus;
+import br.eti.kinoshita.testlinkjavaapi.constants.ExecutionType;
+import br.eti.kinoshita.testlinkjavaapi.constants.TestCaseDetails;
+import br.eti.kinoshita.testlinkjavaapi.model.Build;
+import br.eti.kinoshita.testlinkjavaapi.model.TestCase;
+import br.eti.kinoshita.testlinkjavaapi.model.TestPlan;
+import br.eti.kinoshita.testlinkjavaapi.model.TestProject;
 
 /**
  * Created by breinjhel on 8/11/16.
  */
 @Mojo(name = "report-result")
-public class TestlinkMojo extends AbstractMojo{
+public class TestlinkMojo extends AbstractMojo {
 
-    @Parameter
-    private String url;
+	@Parameter
+	private String url;
 
-    @Parameter
-    private String devKey;
+	@Parameter
+	private String devKey;
 
-    @Parameter
-    private String projectName;
+	@Parameter
+	private String projectName;
 
-    @Parameter
-    private String testPlan;
+	@Parameter
+	private String testPlan;
 
-    @Parameter
-    private String platform;
+	@Parameter
+	private String platform;
 
-    @Parameter(property = "testlink.build")
-    private String build;
+	@Parameter(property = "testlink.build")
+	private String build;
 
-    @Parameter
-    private String outputDir;
+	@Parameter
+	private String outputDir;
 
-    @Parameter
-    private String customField;
+	@Parameter
+	private String customField;
 
-    @Parameter(defaultValue = "TEST-*.xml")
-    private String filename;
+	@Parameter(defaultValue = "TEST-*.xml")
+	private String filename;
 
-    TestLinkAPI api;
-    TestProject testProject;
-    TestPlan tp;
+	TestLinkAPI api;
+	TestProject testProject;
+	TestPlan tp;
 
-    public void execute() throws MojoExecutionException{
-        try {
-            api = new TestLinkAPI(new URL(url), devKey);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
+	@Override
+	public void execute() throws MojoExecutionException {
+		try {
+			this.api = new TestLinkAPI(new URL(this.url), this.devKey);
+		} catch (final MalformedURLException e) {
+			e.printStackTrace();
+		}
 
+		// Get all testcases in testlink that has custom and automated
+		this.testProject = this.api.getTestProjectByName(this.projectName);
+		this.tp = this.api.getTestPlanByName(this.testPlan, this.testProject.getName());
+		List<TestCase> tcs;
 
-        // Get all testcases in testlink that has custom and automated
-        testProject = api.getTestProjectByName(projectName);
-        tp = api.getTestPlanByName(testPlan,testProject.getName());
-        List<TestCase> tcs;
+		getLog().info("TestProject: " + this.testProject.getName());
+		getLog().info("TestPlan: " + this.tp.getName());
 
-        getLog().info("TestProject: " + testProject.getName());
-        getLog().info("TestPlan: " + tp.getName());
+		if (this.platform != null) {
+			tcs = getTestcasesByPlatform(this.tp, this.platform);
+		} else {
+			final TestCase[] testCases = this.api.getTestCasesForTestPlan(this.tp.getId(), null, null, null, null, null, null, null,
+					ExecutionType.AUTOMATED, null, TestCaseDetails.FULL);
+			tcs = Arrays.asList(testCases);
+		}
 
+		final Map<String, List<TestCase>> testcasesPerCustomfields = sortTestcasesByCustomFields(tcs);
+		// Parse xml junit result
+		reportJunitResultsToTestlink(testcasesPerCustomfields);
 
-        if(platform != null){
-            tcs = getTestcasesByPlatform(tp,platform);
-        } else {
-            TestCase[] testCases = this.api.getTestCasesForTestPlan(tp.getId(), null, null, null, null, null, null, null,
-                    ExecutionType.AUTOMATED, null, TestCaseDetails.FULL);
-            tcs = Arrays.asList(testCases);
-        }
+	}
 
-        Map<String, List<TestCase>> testcasesPerCustomfields = sortTestcasesByCustomFields(tcs);
-        // Parse xml junit result
-        reportJunitResultsToTestlink(testcasesPerCustomfields);
+	private Map<String, List<TestCase>> sortTestcasesByCustomFields(List<TestCase> a) {
+		final Map<String, List<TestCase>> b = new HashMap<>();
 
-    }
+		final List<String> foundKeys = new ArrayList<>();
 
-    private Map<String, List<TestCase>> sortTestcasesByCustomFields(List<TestCase> a){
-        Map<String, List<TestCase>> b = new HashMap<>();
+		// Get all keys
+		a.stream().forEach(testCase -> {
+			final String key = this.api.getTestCaseCustomFieldDesignValue(testCase.getId(), null, testCase.getVersion(),
+					this.testProject.getId(), this.customField, null).getValue();
+			if (key != null && !foundKeys.contains(key)) {
+				foundKeys.add(key);
+				b.put(key, new ArrayList<>());
+			}
+			b.get(key).add(testCase);
+		});
 
-        List<String> foundKeys = new ArrayList<>();
+		getLog().info("Found TestCases:");
+		if (b.keySet().isEmpty()) {
+			getLog().info("No TestCases found.");
+		} else {
+			b.keySet().stream().forEach(getLog()::info);
+		}
+		return b;
 
+	}
 
-        // Get all keys
-        a.stream().forEach( testCase -> {
-            String key = api.getTestCaseCustomFieldDesignValue(testCase.getId(), null, testCase.getVersion(), testProject.getId(),customField, null).getValue();
-            if (key !=null&& !foundKeys.contains(key)) {
-                foundKeys.add(key);
-                b.put(key, new ArrayList<>());
-            }
-            b.get(key).add(testCase);
-        });
+	private List<TestCase> getTestcasesByPlatform(TestPlan tp, String platform) {
+		final TestCase[] testCases = this.api.getTestCasesForTestPlan(tp.getId(), null, null, null, null, null, null, null,
+				ExecutionType.AUTOMATED, null, TestCaseDetails.FULL);
 
-        return b;
+		final List<TestCase> s = Arrays.stream(testCases).filter(a -> a.getPlatform().getName().equals(platform))
+				.collect(Collectors.toCollection(ArrayList::new));
 
-    }
+		return s;
+	}
 
-    private List<TestCase> getTestcasesByPlatform(TestPlan tp, String platform){
-        TestCase[] testCases = this.api.getTestCasesForTestPlan(tp.getId(), null, null, null, null, null, null, null,
-                ExecutionType.AUTOMATED, null, TestCaseDetails.FULL);
+	private void reportJunitResultsToTestlink(Map<String, List<TestCase>> sortedTestcases) {
+		final File file = new File(this.outputDir);
+		final FileFilter fileFilter = new WildcardFileFilter(this.filename);
+		final File[] files = file.listFiles(fileFilter);
+		if (files != null) {
+			for (final File f : files) {
+				getLog().info("File - " + f.getName());
+				JAXBContext jaxbContext = null;
+				try {
+					jaxbContext = JAXBContext.newInstance(TestSuite.class);
 
-        List<TestCase> s = Arrays.stream(testCases).filter(a -> a.getPlatform().getName().equals(platform))
-                .collect(Collectors.toCollection(ArrayList::new));
+					final Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+					final TestSuite ts = (TestSuite) jaxbUnmarshaller.unmarshal(f);
+					final com.breinjhel.model.TestCase[] tc = ts.getTestCases();
 
-        return s;
-    }
+					Arrays.stream(tc).forEach(s -> {
+						ExecutionStatus status;
+						final String tcName = s.getClassname() + "#" + s.getName();
 
-    private void reportJunitResultsToTestlink(Map<String, List<TestCase>> sortedTestcases){
-        File file = new File(outputDir);
-        FileFilter fileFilter = new WildcardFileFilter(filename);
-        File[] files = file.listFiles(fileFilter);
-        if(files != null){
-            for(File f: files){
-                getLog().info("File - "+ f.getName());
-                JAXBContext jaxbContext = null;
-                try {
-                    jaxbContext = JAXBContext.newInstance(TestSuite.class);
+						if (s.getError() != null || s.getFailure() != null) {
+							status = ExecutionStatus.FAILED;
+						} else {
+							status = ExecutionStatus.PASSED;
+						}
+						final Build[] builds = this.api.getBuildsForTestPlan(this.tp.getId());
+						if (builds == null || Arrays.asList(builds).stream().noneMatch(o -> o.getName().equals(this.build))) {
+							this.api.createBuild(this.tp.getId(), this.build, "Created by Testlink Maven Plugin");
+						}
 
-                    Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-                    TestSuite ts = (TestSuite) jaxbUnmarshaller.unmarshal(f);
-                    com.breinjhel.model.TestCase[] tc = ts.getTestCases();
+						if (sortedTestcases.containsKey(tcName)) {
 
+							sortedTestcases.get(tcName).forEach(j -> {
+								if (s.getError() != null || s.getFailure() != null) {
+									final String note = s.getError() != null ? s.getError().getMessage() : s.getFailure().getMessage();
+									getLog().info(tcName + " - Failed");
+									this.api.reportTCResult(j.getId(), null, this.tp.getId(), status, null, this.build, note, null, null,
+											null, this.platform, null, true);
+								} else {
+									getLog().info(tcName + " - Passed");
+									this.api.reportTCResult(j.getId(), null, this.tp.getId(), status, null, this.build, "", null, null,
+											null, this.platform, null, true);
 
-                    Arrays.stream(tc).forEach(s -> {
-                        ExecutionStatus status;
-                        String tcName = s.getClassname() + "#" + s.getName();
+								}
 
-                        if(s.getError() != null || s.getFailure() != null){
-                            status = ExecutionStatus.FAILED;
-                        } else {
-                            status = ExecutionStatus.PASSED;
-                        }
-                        Build[] builds = api.getBuildsForTestPlan(tp.getId());
-                        if(!Arrays.asList(builds).stream().filter(o -> o.getName().equals(build)).findFirst().isPresent()){
-                            api.createBuild(tp.getId(),build,"Created by Testlink Maven Plugin");
-                        }
-
-                        if(sortedTestcases.containsKey(tcName)){
-
-                            sortedTestcases.get(tcName).forEach( j -> {
-                                if(s.getError() != null || s.getFailure() != null){
-                                    String note = s.getError() != null ? s.getError().getMessage() : s.getFailure().getMessage();
-                                    getLog().info(tcName + " - Failed");
-                                    this.api.reportTCResult(j.getId(), null, tp.getId(), status,null, build, note, null,null,null,platform,null,true);
-                                } else {
-                                    getLog().info(tcName + " - Passed");
-                                    this.api.reportTCResult(j.getId(), null, tp.getId(), status,null, build, "", null,null,null,platform,null,true);
-
-                                }
-
-                            });
-                        } else {
-                            getLog().info("No test case found for: " + tcName);
-                        }
-                    });
-                } catch (JAXBException e) {
-                    e.printStackTrace();
-                }
-            }
-        } else {
-            getLog().warn("No test results found in "+ outputDir + "/" + filename);
-        }
-    }
+							});
+						} else {
+							getLog().info("No test case found for: " + tcName);
+						}
+					});
+				} catch (final JAXBException e) {
+					e.printStackTrace();
+				}
+			}
+		} else {
+			getLog().warn("No test results found in " + this.outputDir + "/" + this.filename);
+		}
+	}
 
 }
